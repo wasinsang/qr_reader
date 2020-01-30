6774ab04a6ac2ba526934a5bbc933f1c188a5c67
wane_GBT <wasinsang@gmail.com>
2020-01-30 23:51:39 +0700

    motion

--- motion.py
+++ motion.py
@@ -1,36 +1,78 @@
-import os
-import pyzbar.pyzbar as pyzbar
+# import the necessary packages
+from imutils.video import VideoStream
+import argparse
+import datetime
+import imutils
+import time
 import cv2
-import numpy as np
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-camera_type = 'picamera'
-IM_WIDTH = 1280
-IM_HEIGHT = 720
-font = cv2.FONT_HERSHEY_PLAIN
-frame_rate_calc = 1
-freq = cv2.getTickFrequency()
-font = cv2.FONT_HERSHEY_SIMPLEX
-camera = PiCamera()
-camera.resolution = (IM_WIDTH,IM_HEIGHT)
-camera.framerate = 10
-rawCapture = PiRGBArray(camera, size=(IM_WIDTH,IM_HEIGHT))
-rawCapture.truncate(0)
-FirstFrame = None
-for frame1 in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
-    frame = np.copy(frame1.array)
-    frame.setflags(write=1)
-    frame_rgb = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-    frame_expanded = np.expand_dims(frame_rgb,axis=0)
-    
-#     decodeObjects = pyzbar.decode(frame)
-#     for obj in decodeObjects:
-#         print ("Data",obj.data)
-#         cv2.putText(frame, str(obj.data), (50,50), font, 2,(255,0,0),3)
-#     cv2.imshow("Frame",frame)
 
-    if cv2.waitKey(1)== ord('q'):
-        break
-    rawCapture.truncate(0)
+# construct the argument parser and parse the arguments
+vs = cv2.VideoCapture(0)
 
+# initialize the first frame in the video stream
+firstFrame = None
+# loop over the frames of the video
+while True:
+	# grab the current frame and initialize the occupied/unoccupied
+	# text
+	frame = vs.read()
+	frame = frame if ("vs", None) is None else frame[1]
+	text = "Unoccupied"
 
+	# if the frame could not be grabbed, then we have reached the end
+	# of the video
+	if frame is None:
+		break
+
+	# resize the frame, convert it to grayscale, and blur it
+	frame = imutils.resize(frame, width=500)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+	# if the first frame is None, initialize it
+	if firstFrame is None:
+		firstFrame = gray
+		continue
+		# compute the absolute difference between the current frame and
+	# first frame
+	frameDelta = cv2.absdiff(firstFrame, gray)
+	thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+
+	# dilate the thresholded image to fill in holes, then find contours
+	# on thresholded image
+	thresh = cv2.dilate(thresh, None, iterations=2)
+	cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)
+	cnts = imutils.grab_contours(cnts)
+
+	# loop over the contours
+	for c in cnts:
+		# if the contour is too small, ignore it
+		if cv2.contourArea(c) < 500:
+			continue
+
+		# compute the bounding box for the contour, draw it on the frame,
+		# and update the text
+		(x, y, w, h) = cv2.boundingRect(c)
+		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+		text = "Occupied"
+		# draw the text and timestamp on the frame
+	cv2.putText(frame, "Motion Status: {}".format(text), (10, 20),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+	print(text)
+	cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
+		(10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+
+	# show the frame and record if the user presses a key
+	cv2.imshow("Security Feed", frame)
+	cv2.imshow("Thresh", thresh)
+	cv2.imshow("Frame Delta", frameDelta)
+	key = cv2.waitKey(1) & 0xFF
+        
+	# if the `q` key is pressed, break from the lop
+	if key == ord("q"):
+		break
+
+# cleanup the camera and close any open windows
+vs.stop() if ("vs", None) is None else vs.release()
+cv2.destroyAllWindows()
