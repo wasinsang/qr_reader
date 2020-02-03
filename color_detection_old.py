09767b029d06e64b54ff011089ed3847c093bc79
wane_GBT <wasinsang@gmail.com>
2020-02-03 11:32:21 +0700

    add color_detection

--- color_detection_old.py
+++ color_detection_old.py
@@ -1,16 +1,9 @@
 import cv2
 import numpy as np
 import pandas as pd
-import argparse
 
-#Creating argument parser to take image path from command line
-ap = argparse.ArgumentParser()
-ap.add_argument('-i', '--image', required=True, help="Image Path")
-args = vars(ap.parse_args())
-img_path = args['image']
 
-#Reading the image with opencv
-img = cv2.imread(img_path)
+img = cv2.imread("symbol.jpg")
 
 #declaring global variables (are used later on)
 clicked = False
@@ -48,22 +41,22 @@ cv2.setMouseCallback('image',draw_function)
 while(1):
 
     cv2.imshow("image",img)
-
+    if (clicked):
    
-    #cv2.rectangle(image, startpoint, endpoint, color, thickness)-1 fills entire rectangle 
-    cv2.rectangle(img,(20,20), (750,60), (b,g,r), -1)
+        #cv2.rectangle(image, startpoint, endpoint, color, thickness)-1 fills entire rectangle 
+        cv2.rectangle(img,(20,20), (750,60), (b,g,r), -1)
 
         #Creating text string to display( Color name and RGB values )
-    text = getColorName(r,g,b) + ' R='+ str(r) +  ' G='+ str(g) +  ' B='+ str(b)
+        text = getColorName(r,g,b) + ' R='+ str(r) +  ' G='+ str(g) +  ' B='+ str(b)
         
         #cv2.putText(img,text,start,font(0-7),fontScale,color,thickness,lineType )
-    cv2.putText(img, text,(50,50),2,0.8,(255,255,255),2,cv2.LINE_AA)
+        cv2.putText(img, text,(50,50),2,0.8,(255,255,255),2,cv2.LINE_AA)
 
         #For very light colours we will display text in black colour
-    if(r+g+b>=600):
-        cv2.putText(img, text,(50,50),2,0.8,(0,0,0),2,cv2.LINE_AA)
+        if(r+g+b>=600):
+            cv2.putText(img, text,(50,50),2,0.8,(0,0,0),2,cv2.LINE_AA)
             
-
+        clicked=False
 
     #Break the loop when user hits 'esc' key    
     if cv2.waitKey(20) & 0xFF ==27:
